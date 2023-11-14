503a03ee881504a7141e0f98f1de703091374336
MathieuCoding <polletmathieu.pro@gmail.com>
2023-11-14 15:06:16 +0100

    ex2 exception added

--- venv/exo/ex2.py
+++ venv/exo/ex2.py
@@ -1,7 +1,10 @@
 # Manipulation de chaînes : Demandez à l'utilisateur de saisir une phrase, puis affichez la phrase en majuscules, en minuscules et le nombre de mots.
 def display_input(user_input):
-    print(user_input.upper())
-    print(user_input.lower())
-    print('There are ', user_input.count(' ') + 1, ' words in your sentence.')
+    if len(user_input) == 0:
+        print('There are no words in this sentence')
+    else:
+        print(user_input.upper())
+        print(user_input.lower())
+        print('There are ', user_input.count(' ') + 1, ' words in your sentence.')
 
 display_input(input("Enter a sentence: "))
--- venv/exo/ex3.py
+++ venv/exo/ex3.py
@@ -1,2 +1,4 @@
 # Calculatrice simple : Écrivez un programme qui prend deux nombres et effectue les opérations de base : addition, soustraction, multiplication, division.
 # ex : “3+1-4x4”
+
+def calculate(user_input):
